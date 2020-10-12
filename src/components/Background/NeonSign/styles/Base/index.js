f9bb3589d6706b363ea1846a4e097d4155c13184
Heisey <jheisler01@gmail.com>
2020-10-12 16:37:49 -0700

    finished bottom row of lights

--- src/components/Background/NeonSign/styles/Base/index.js
+++ src/components/Background/NeonSign/styles/Base/index.js
@@ -59,34 +59,29 @@ export const BoardFront = styled.div`
     132px 15px 0 0 white, 131px 15px 0 -1px white, 130px 16px 0 -2px white, 129px 16px 0 -2px white, 129px 16px 3px -2px grey, 
     163px 15px 0 0 white, 162px 15px 0 -1px white, 161px 16px 0 -2px white, 160px 16px 0 -2px white, 160px 16px 3px -2px grey,
   
+    /* Right Row */
     163px 43px 0 0 white, 162px 43px 0 -1px white, 161px 44px 0 -2px white, 160px 44px 0 -2px white, 160px 44px 3px -2px grey, 
     163px 71px 0 0 white, 162px 71px 0 -1px white, 161px 72px 0 -2px white, 160px 72px 0 -2px white, 160px 72px 3px -2px grey, 
     163px 99px 0 0 white, 162px 99px 0 -1px white, 161px 100px 0 -2px white, 160px 100px 0 -2px white, 160px 100px 3px -2px grey, 
-    163px 127px 0 0 white, 162px 127px 0 -1px white, 161px 128px 0 -2px white, 160px 128px 0 -2px white, 160px 128px 3px -2px grey, 
-    163px 155px 0 0 white, 162px 155px 0 -1px white, 161px 156px 0 -2px white, 160px 156px 0 -2px white, 160px 156px 3px -2px grey,
+    163px 127px 0 0 white, 162px 127px 0 -1px white, 161px 128px 0 -2px white, 160px 128px 0 -2px white, 160px 128px 3px -2px grey,
 
-    480px 165px 0 0 white, 479px 165px 0 -1px white, 478px 166px 0 -2px white, 477px 166px 0 -2px white, 477px 166px 3px -2px grey, 
-    450px 165px 0 0 white, 449px 165px 0 -1px white, 448px 166px 0 -2px white, 447px 166px 0 -2px white, 447px 166px 3px -2px grey, 
-    420px 165px 0 0 white, 419px 165px 0 -1px white, 418px 166px 0 -2px white, 417px 166px 0 -2px white, 417px 166px 3px -2px grey, 
-    390px 165px 0 0 white, 389px 165px 0 -1px white, 388px 166px 0 -2px white, 387px 166px 0 -2px white, 387px 166px 3px -2px grey, 
-    360px 165px 0 0 white, 359px 165px 0 -1px white, 358px 166px 0 -2px white, 357px 166px 0 -2px white, 357px 166px 3px -2px grey, 
-    330px 165px 0 0 white, 329px 165px 0 -1px white, 328px 166px 0 -2px white, 327px 166px 0 -2px white, 327px 166px 3px -2px grey, 
-    300px 165px 0 0 white, 299px 165px 0 -1px white, 298px 166px 0 -2px white, 297px 166px 0 -2px white, 297px 166px 3px -2px grey, 
-    270px 165px 0 0 white, 269px 165px 0 -1px white, 268px 166px 0 -2px white, 267px 166px 0 -2px white, 267px 166px 3px -2px grey, 
-    240px 165px 0 0 white, 239px 165px 0 -1px white, 238px 166px 0 -2px white, 237px 166px 0 -2px white, 237px 166px 3px -2px grey, 
-    210px 165px 0 0 white, 209px 165px 0 -1px white, 208px 166px 0 -2px white, 207px 166px 0 -2px white, 207px 166px 3px -2px grey, 
-    /* 180px 165px 0 0 white, 179px 165px 0 -1px white, 178px 166px 0 -2px white, 177px 166px 0 -2px white, 177px 166px 3px -2px grey,  */
-    /* 150px 165px 0 0 white, 149px 165px 0 -1px white, 148px 166px 0 -2px white, 147px 166px 0 -2px white, 147px 166px 3px -2px grey,  */
-    120px 165px 0 0 white, 119px 165px 0 -1px white, 118px 166px 0 -2px white, 117px 166px 0 -2px white, 117px 166px 3px -2px grey, 
-    90px 165px 0 0 white, 89px 165px 0 -1px white, 88px 166px 0 -2px white, 87px 166px 0 -2px white, 87px 166px 3px -2px grey, 
-    60px 165px 0 0 white, 59px 165px 0 -1px white, 58px 166px 0 -2px white, 57px 166px 0 -2px white, 57px 166px 3px -2px grey, 
-    30px 165px 0 0 white, 29px 165px 0 -1px white, 28px 166px 0 -2px white, 27px 166px 0 -2px white, 27px 166px 3px -2px grey, 
-    0px 165px 0 0 white, -1px 165px 0 -1px white, -2px 166px 0 -2px white, -3px 166px 0 -2px white, -3px 166px 3px -2px grey, 
-    -30px 165px 0 0 white, -31px 165px 0 -1px white, -32px 166px 0 -2px white, -33px 166px 0 -2px white, -33px 166px 3px -2px grey,
+    /* Bottom Row */
+    163px 155px 0 0 white, 162px 155px 0 -1px white, 161px 156px 0 -2px white, 160px 156px 0 -2px white, 160px 156px 3px -2px grey,
+    132px 155px 0 0 white, 131px 155px 0 -1px white, 130px 156px 0 -2px white, 129px 156px 0 -2px white, 29px 156px 3px -2px grey, 
+    101px 155px 0 0 white, 100px 155px 0 -1px white, 99px 156px 0 -2px white, 98px 156px 0 -2px white, 98px 156px 3px -2px grey, 
+    70px 155px 0 0 white, 69px 155px 0 -1px white, 68px 156px 0 -2px white, 67px 156px 0 -2px white, 67px 156px 3px -2px grey, 
+    39px 155px 0 0 white, 38px 155px 0 -1px white, 37px 156px 0 -2px white, 36px 156px 0 -2px white, 36px 156px 3px -2px grey, 
+    2px 155px 0 0 white, 1px 155px 0 -1px white, 0 156px 0 -2px white, -1px 156px 0 -2px white, -1px 156px 3px -2px grey, 
+    -29px 155px 0 0 white, -28px 155px 0 -1px white, -27px 156px 0 -2px white, -26px 156px 0 -2px white, -26px 156px 3px -2px grey, 
+    -60px 155px 0 0 white, -59px 155px 0 -1px white, -58px 156px 0 -2px white, -57px 156px 0 -2px white, -57px 156px 3px -2px grey, 
+    -91px 155px 0 0 white, -90px 155px 0 -1px white, -89px 156px 0 -2px white, -88px 156px 0 -2px white, -88px 156px 3px -2px grey, 
+    -122px 155px 0 0 white, -121px 155px 0 -1px white, -120px 156px 0 -2px white, -119px 156px 0 -2px white, -119px 156px 3px -2px grey, 
+    -153px 155px 0 0 white, -154px 155px 0 -1px white, -155px 156px 0 -2px white, -156px 156px 0 -2px white, -156px 156px 3px -2px grey, 
+    -184px 155px 0 0 white, -85px 155px 0 -1px white, -186px 156px 0 -2px white, -187px 156px 0 -2px white, -187px 156px 3px -2px grey, 
 
     -30px 135px 0 0 white, -31px 135px 0 -1px white, -32px 136px 0 -2px white, -33px 136px 0 -2px white, -33px 136px 3px -2px grey, 
     -30px 105px 0 0 white, -31px 105px 0 -1px white, -32px 106px 0 -2px white, -33px 106px 0 -2px white, -33px 106px 3px -2px grey, 
-    /* -30px 75px 0 0 white, -31px 75px 0 -1px white, -32px 76px 0 -2px white, -33px 76px 0 -2px white, -33px 76px 3px -2px grey,  */
+    -30px 75px 0 0 white, -31px 75px 0 -1px white, -32px 76px 0 -2px white, -33px 76px 0 -2px white, -33px 76px 3px -2px grey, 
     -30px 45px 0 0 white, -31px 45px 0 -1px white, -32px 46px 0 -2px white, -33px 46px 0 -2px white, -33px 46px 3px -2px grey;
 }
 `
